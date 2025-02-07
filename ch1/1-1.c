9ff41ef18b7621c9cd3ebaf2d83a6db35fdd4e56
chenyinjie <chenyinjie666@foxmail.com>
2025-02-07 21:23:38 +0800

    2025/02/07 add ch1 1-1.c

---
+++ ch1/1-1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    printf("\\a: Hello, World!\a");
+    printf("\\b: Hello, World!\b");
+    printf("\\f: Hello, World!\f");
+    printf("\\n: Hello, World!\n");
+    printf("\\r: Hello, World!\r");
+    printf("\\t: Hello, World!\t");
+    printf("\\v: Hello, World!\v");
+
+    return 0;
+}
